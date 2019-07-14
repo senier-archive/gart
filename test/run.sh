469b671737cfadcacc91b0802112fa4072a3df2a
Alexander Senier <senier@componolit.com>
2019-07-14 23:00:15 +0200

    Add base-linux x86_64 tests

--- test/run.sh
+++ test/run.sh
@@ -1,8 +1,44 @@
 #!/bin/sh -eu
 
-apt update
-apt -y install python3-pip
-pip3 install pyparsing
+GENODE_DIR=/genode
 
+# Install pyparsing
+apt update
+apt -y install python3-pyparsing
+
+# Test gnoos
 cd /gart/tool
 ./gnoos --test
+
+# Setup external repos
+git clone https://github.com/genodelabs/genode-world.git ${GENODE_DIR}/repos/world
+ln -sf /gart ${GENODE_DIR}/repos/gart
+
+# Configure build
+mkdir -p /genode_build/etc
+ln -sf ${GENODE_DIR}/tool/builddir/build.mk /genode_build/Makefile
+echo 'SPECS += x86_64' > /genode_build/etc/specs.conf
+
+cat << EOF > /genode_build/etc/build.conf
+GENODE_DIR := ${GENODE_DIR}
+BASE_DIR := ${GENODE_DIR}/repos/base
+CONTRIB_DIR := ${GENODE_DIR}/contrib
+
+MAKE += -j8
+KERNEL = linux
+
+RUN_OPT += --include power_on/linux --include log/linux --include boot_dir/linux
+
+REPOSITORIES += ${GENODE_DIR}/repos/base-linux
+REPOSITORIES += ${GENODE_DIR}/repos/base
+REPOSITORIES += ${GENODE_DIR}/repos/os
+REPOSITORIES += ${GENODE_DIR}/repos/libports
+REPOSITORIES += ${GENODE_DIR}/repos/ports
+REPOSITORIES += ${GENODE_DIR}/repos/world
+REPOSITORIES += ${GENODE_DIR}/repos/gart
+EOF
+
+# Prepare ports
+/genode/tool/ports/prepare_port googletest
+
+make -C /genode_build run/log
