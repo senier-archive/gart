ed2cf5e028eae596a241ada5ac601b6943f93ea9
Alexander Senier <senier@componolit.com>
2019-08-22 15:26:21 +0200

    Improve debug output for system properties
    Ref.: #8

--- include/gart/sys/_system_properties.h
+++ include/gart/sys/_system_properties.h
@@ -11,45 +11,45 @@
 typedef struct prop_info { } prop_info;
 
 static uint32_t __system_property_serial(const prop_info* __pi) {
-   printf("%s() not implemented", __func__);
+   printf("%s() not implemented\n", __func__);
    return 0;
 };
 
 static const prop_info* __system_property_find(const char* __name) {
-   printf("%s(__name=%s) not implemented", __func__, __name);
+   printf("%s(name=\"%s\") not implemented\n", __func__, __name);
    return NULL;
 };
 
 static int __system_property_read(const prop_info* __pi, char* __name, char* __value) {
-   printf("%s(__name=%s, __value=%s) not implemented", __func__, __name, __value);
-   return 0;
+   printf("%s(name=\"%s\", value=\"%s\") not implemented\n", __func__, __name, __value);
+   return -1;
 };
 
 static uint32_t __system_property_area_serial(void) {
-   printf("%s() not implemented", __func__);
-   return 0;
+   printf("%s() not implemented\n", __func__);
+   return -1;
 };
 
 static int __system_property_get(const char *__name, char *__value) {
-   printf("%s(__name=%s, __value=%s) not implemented", __func__, __name, __value);
-   return 0;
+   printf("%s(name=\"%s\", value=\"%s\") not implemented\n", __func__, __name, __value);
+   return -1;
 };
 
 static int __system_property_set(const char *__name, const char *__value) {
-   printf("%s(__name=%s, __value=%s) not implemented", __func__, __name, __value);
-   return 0;
+   printf("%s(name=\"%s\", value=\"%s\") not implemented\n", __func__, __name, __value);
+   return -1;
 };
 
 static void __system_property_read_callback
    (const prop_info* __pi,
     void (*__callback)(void* __cookie, const char* __name, const char* __value, uint32_t __serial),
     void* __cookie) {
-   printf("%s not implemented", __func__);
+   printf("%s() not implemented\n", __func__);
 }
 
 static int __system_property_foreach(void (*__callback)(const prop_info* __pi, void* __cookie), void* __cookie) {
-   printf("%s not implemented", __func__);
-   return 0;
+   printf("%s() not implemented\n", __func__);
+   return -1;
 }
 
 #endif // !__GART_SYS__SYSTEM_PROPERTIES_H__
