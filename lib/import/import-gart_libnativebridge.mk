0ce5b17897a38ffd5d04eb707737b23b7c83b4ad
Alexander Senier <senier@componolit.com>
2019-08-25 00:47:08 +0200

    Port libnativebridge
    Ref. #21

---
+++ lib/import/import-gart_libnativebridge.mk
@@ -0,0 +1,3 @@
+ANDROID_LIB = libnativebridge
+include $(call select_from_repositories,lib/import/import-android.inc)
+
--- lib/mk/gart_libnativebridge.mk
+++ lib/mk/gart_libnativebridge.mk
@@ -1,6 +1,9 @@
 ANDROID_PORT		= core
 ANDROID_DIR			= system/core/libnativebridge
 ANDROID_SECTIONS	= /cc_library[@name=libnativebridge]
-#ANDROID_EXCLUDE_OPT += -fvisibility=protected
+ANDROID_LIBS      = libnativehelper
 
 include $(call select_from_repositories,lib/mk/android-lib.inc)
+
+# Pretend we're on apple, as this disable the use of bound mounts
+CC_OPT_native_bridge += -D__APPLE__=1 -Wno-return-type
