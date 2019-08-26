6b7967098a3c6e4080c62df173680a5539e63319
Alexander Senier <senier@componolit.com>
2019-08-26 10:26:09 +0200

    Handle invalid configuration of main program

--- src/main/main.cc
+++ src/main/main.cc
@@ -1,6 +1,7 @@
 /* Genode includes */
 #include <libc/component.h>
 #include <base/attached_rom_dataspace.h>
+#include <util/xml_node.h>
 
 /* libc includes */
 #include <stdio.h>
@@ -21,12 +22,18 @@ void Libc::Component::construct(Libc::Env &env)
        int argc = 0;
 		 const char *argv[MAX_ARGS];
 
-       auto args = config.xml().sub_node("start");
-       argv[argc++] = strdup(args.attribute_value("name", Value("/bin/unknown")).string());
+       try {
+         auto args = config.xml().sub_node("start");
+         argv[argc++] = strdup(args.attribute_value("name", Value("/bin/unknown")).string());
 
-       args.for_each_sub_node("arg", [&] (Genode::Xml_node arg_node) {
-           argv[argc++] = strdup(arg_node.attribute_value("value", Value("ERROR")).string());
-       });
+         args.for_each_sub_node("arg", [&] (Genode::Xml_node arg_node) {
+             argv[argc++] = strdup(arg_node.attribute_value("value", Value("ERROR")).string());
+         });
+       } catch (Genode::Xml_node::Nonexistent_sub_node) {
+         fprintf(stderr, "Invalid main() configuration, add <start> node\n");
+         exit (-1);
+       }
+
        argv[argc] = 0;
 
        setprogname (argv[0]);
