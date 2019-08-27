0c7793104e8263531e3d259888ed2d09173c15c6
Alexander Senier <senier@componolit.com>
2019-08-27 15:17:50 +0200

    Mute old-style-cast warnings

--- lib/mk/gart_libziparchive.mk
+++ lib/mk/gart_libziparchive.mk
@@ -3,5 +3,6 @@ ANDROID_DIR			= system/core/libziparchive
 ANDROID_SECTIONS	= \
 	/cc_library[@name=libziparchive] \
 	/cc_library[@name=libziparchive]/target/android
+ANDROID_EXCLUDE_OPT = -Wold-style-cast
 
 include $(call select_from_repositories,lib/mk/android-lib.inc)
--- src/test/libziparchive/target.mk
+++ src/test/libziparchive/target.mk
@@ -3,6 +3,7 @@ TARGET = libziparchive_test
 ANDROID_PORT	  = core
 ANDROID_DIR		  = system/core/libziparchive
 ANDROID_SECTIONS = /cc_test[@name=ziparchive-tests]
+ANDROID_EXCLUDE_OPT = -Wold-style-cast
 
 include $(call select_from_repositories,lib/mk/android-prg.inc)
 
