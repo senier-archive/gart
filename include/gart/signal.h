98677fab4b45e5de16f55d58136f922ad53f25ad
Alexander Senier <senier@componolit.com>
2019-08-27 13:31:50 +0200

    Fail if signal is raised

--- include/gart/signal.h
+++ include/gart/signal.h
@@ -2,8 +2,18 @@
 #define __GART_SIGNAL_H__
 
 #include <sys/signal.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-static int sigemptyset(sigset_t *set) { return -1; };
-static int sigaction(int signum, const struct sigaction *act, struct sigaction *oldact) { return -1; };
+static int sigemptyset(sigset_t *set) { return -1; }
+static int sigaction(int signum, const struct sigaction *act, struct sigaction *oldact) { return -1; }
+
+static
+void raise_SIGINT() {
+   fprintf(stderr, "raise(SIGINT) not implemented");
+   exit(1);
+}
+
+#define raise(sig) raise_##sig()
 
 #endif // !__GART_SIGNAL_H__
