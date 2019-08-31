d54419aa9c69ceff371b6c5fd78095861f45192b
Alexander Senier <senier@componolit.com>
2019-08-31 20:37:36 +0200

    Supress logging only for libart

--- include/gart/android-base/logging.h
+++ include/gart/android-base/logging.h
@@ -3,10 +3,12 @@
 #include_next <android-base/logging.h>
 #include <gart/log_discard.h>
 
+#ifdef ART_LOG_CONSTEXPR_HACK
+
 #undef CHECK_OP
 #define CHECK_OP(LHS, RHS, OP) Discard()
 
-#ifdef ART_LOG_CONSTEXPR_HACK
 #undef WOULD_LOG
 #define WOULD_LOG(severity) MUST_LOG_MESSAGE(severity)
+
 #endif // ART_LOG_CONSTEXPR_HACK
