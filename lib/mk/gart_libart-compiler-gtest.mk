4c1903da99929a155973ad7dc9b38dc769410b23
Alexander Senier <senier@componolit.com>
2019-09-02 19:20:36 +0200

    Port libart test dependencies
    Ref. #50

---
+++ lib/mk/gart_libart-compiler-gtest.mk
@@ -0,0 +1,29 @@
+ANDROID_PORT       = art
+ANDROID_DIR        = art
+ANDROID_SOURCES    = compiler
+ANDROID_BUILDFILES = $(REP_DIR)/include/vixl/Android.bp Android.bp test/Android.bp build/Android.bp compiler/Android.bp
+ANDROID_SECTIONS   = /art_cc_library[@name=libart-compiler-gtest]
+
+ANDROID_EXCLUDE_LIBS += libartd
+ANDROID_LIBS += libart
+
+ANDROID_EXCLUDE_LIBS += libartd-compiler
+ANDROID_LIBS += libart-compiler
+
+ANDROID_EXCLUDE_LIBS += libartd-disassembler
+ANDROID_LIBS += libart-disassembler
+
+ANDROID_EXCLUDE_LIBS += libdexfiled
+ANDROID_LIBS += libdexfile
+
+ANDROID_EXCLUDE_LIBS += libgtest
+LIBS += gtest
+
+# jni.h required
+ANDROID_LIBS += libnativehelper
+
+# dlmalloc required
+ANDROID_LIBS += dlmalloc
+
+include $(REP_DIR)/lib/mk/art_opts.inc
+include $(call select_from_repositories,lib/mk/android-lib.inc)
---
+++ lib/mk/gart_libart-runtime-gtest.mk
@@ -0,0 +1,31 @@
+ANDROID_PORT       = art
+ANDROID_DIR        = art
+ANDROID_SOURCES    = runtime
+ANDROID_BUILDFILES = $(REP_DIR)/include/vixl/Android.bp Android.bp test/Android.bp build/Android.bp runtime/Android.bp
+ANDROID_SECTIONS   = /art_cc_library[@name=libart-runtime-gtest]
+
+ANDROID_EXCLUDE_LIBS += libartd
+ANDROID_LIBS += libart
+
+ANDROID_EXCLUDE_LIBS += libartd-compiler
+ANDROID_LIBS += libart-compiler
+
+ANDROID_EXCLUDE_LIBS += libdexfiled
+ANDROID_LIBS += libdexfile
+
+ANDROID_EXCLUDE_LIBS += libgtest
+LIBS += gtest
+
+# jni.h required
+ANDROID_LIBS += libnativehelper
+
+# dlmalloc required
+ANDROID_LIBS += dlmalloc
+
+# unicode required
+ANDROID_LIBS += libicuuc
+
+CC_OPT += -DART_TARGET_NATIVETEST_DIR="/test"
+
+include $(REP_DIR)/lib/mk/art_opts.inc
+include $(call select_from_repositories,lib/mk/android-lib.inc)
