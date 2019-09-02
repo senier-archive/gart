fee8b6289ec572a091b834b67b4def9197823893
Alexander Senier <senier@componolit.com>
2019-09-02 07:09:02 +0200

    Mock more non-portable pthread functions
    Ref. #29

--- include/gart/pthread.h
+++ include/gart/pthread.h
@@ -8,6 +8,16 @@ static pid_t pthread_gettid_np(pthread_t thread) {
    return -1;
 };
 
-static void pthread_setname_np(const char *name) {
+static int pthread_setname_np(pthread_t thread, const char *name) {
    fprintf(stderr, "%s NOT IMPLEMENTED (%s)", __func__, name);
+   return -1;
+}
+static int pthread_getname_np(pthread_t thread, char *name, size_t len) {
+   fprintf(stderr, "%s NOT IMPLEMENTED", __func__);
+   return -1;
+}
+
+static int pthread_getattr_np(pthread_t thread, pthread_attr_t *attrs) {
+   fprintf(stderr, "%s NOT IMPLEMENTED", __func__);
+   return -1;
 }
