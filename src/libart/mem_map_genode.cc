9990fffab874ae77de67c5969116c38250007030
Alexander Senier <senier@componolit.com>
2019-09-16 22:33:20 +0200

    Implement MemMap and MadviseDontNeedAndZero
    Ref. #64

--- src/libart/mem_map_genode.cc
+++ src/libart/mem_map_genode.cc
@@ -1,5 +1,7 @@
 #include <base/log.h>
 #include <base/exception.h>
+#include <dataspace/client.h>
+#include <util/string.h>
 
 #include <gart/env.h>
 #include <android-base/stringprintf.h>
@@ -11,7 +13,66 @@ class Assertion_failed : Genode::Exception { };
 
 namespace art {
 
-    enum { PROT_READ = 1, PROT_WRITE = 2, PROT_EXEC = 4 };
+    class Error
+    {
+        private:
+            const std::string &message_;
+        public:
+            Error(const std::string &message) : message_(message) { }
+            void message(std::string *m) { if (m != nullptr) { *m = message_; } }
+    };
+
+    enum { PROT_READ = 1, PROT_WRITE = 2, PROT_EXEC = 4, PROT_LOWMEM = 8 };
+
+    MemMap::MemMap(const std::string& name,
+                   uint8_t* begin,
+                   size_t size,
+                   void* base_begin,
+                   size_t base_size,
+                   int prot,
+                   bool reuse,
+                   size_t redzone_size) : name_(name)
+                                        , prot_(prot)
+                                        , reuse_(reuse)
+                                        , already_unmapped_(false)
+                                        , redzone_size_(redzone_size)
+                                        , env_(gart::genode_env())
+                                        , ram_ds_cap_(env_.ram().alloc(size))
+                                        , address_space_(env_.pd().address_space())
+    {
+        assert ((prot & ~0x15UL) == 0, "Unsupported protection bits");
+        assert (reuse == 0, "Reuse not implemented");
+
+        bool lowmem = (prot && PROT_LOWMEM) > 0;
+
+        if (!(prot && PROT_READ))
+        {
+            throw Error("Non-accessible mapping unsupported");
+        }
+
+        if ((reinterpret_cast<unsigned long>(begin) & 0xfff) != 0)
+        {
+            throw Error("Address not page-aligned");
+        };
+
+        Genode::Region_map::Local_addr result = address_space_.attach(/* ds */ ram_ds_cap_,
+                                                                      /* size */ 0,
+                                                                      /* offset */ 0,
+                                                                      /* use_local_addr */ begin != nullptr,
+                                                                      /* local_addr */ begin,
+                                                                      /* executable */ prot && PROT_EXEC,
+                                                                      /* writeable */ prot && PROT_WRITE);
+
+        begin_ = result;
+
+        Genode::Dataspace_client ds(ram_ds_cap_);
+        size_ = ds.size();
+
+        base_begin_ = (void *)begin;
+        base_size_ = size_;
+
+        Genode::log("Mapped \"", name.c_str(), "\" (", size, ") to ", Genode::Hex(result));
+    };
 
     MemMap::~MemMap()
     {
@@ -27,37 +88,27 @@ namespace art {
                                  std::string* error_msg,
                                  bool use_ashmem)
     {
-        Genode::Env &_env = gart::genode_env();
-
-        assert ((prot & ~0x7UL) == 0, "Unsupported protection bits");
-        assert (reuse == 0, "Reuse not implemented");
-
-        if ((reinterpret_cast<unsigned long>(addr) & 0xfff) != 0)
+        try
         {
-            if (error_msg != nullptr)
-            {
-                *error_msg = android::base::StringPrintf("Address not page-aligned");
-            }
+            auto result = new MemMap (/* name         */ name,
+                                      /* begin        */ addr,
+                                      /* size         */ byte_count,
+                                      /* base_begin   */ addr,
+                                      /* base_size    */ byte_count,
+                                      /* prot         */ prot || (low_4gb ? PROT_LOWMEM : 0),
+                                      /* reuse        */ reuse,
+                                      /* redzone_size */ 0);
+
+            assert ((reinterpret_cast<uintptr_t>(result->BaseBegin()) >> 32) == 0
+                    && (reinterpret_cast<uintptr_t>(result->BaseEnd()) >> 32) == 0,
+                    "low_4gb not honored");
+
+            return result;
+        } catch (Error e)
+        {
+            e.message(error_msg);
             return nullptr;
-        };
-
-
-        bool prot_read  = prot && PROT_READ;
-        bool prot_write = prot && PROT_WRITE;
-        bool prot_exec  = prot && PROT_EXEC;
-
-        Genode::warning(__PRETTY_FUNCTION__, ": not implemented");
-        Genode::warning("    name=", name);
-        Genode::warning("    addr=", Genode::Hex((unsigned long long)addr));
-        Genode::warning("    count=", byte_count);
-        Genode::warning("    read=", prot_read);
-        Genode::warning("    write=", prot_write);
-        Genode::warning("    exec=", prot_exec);
-        Genode::warning("    low_4gb=", low_4gb);
-        Genode::warning("    reuse=", reuse);
-        Genode::warning("    error_msg=", error_msg != nullptr);
-        Genode::warning("    use_ashmem=", use_ashmem);
-        return nullptr;
+        }
     }
 
     MemMap* MemMap::RemapAtEnd(uint8_t* new_end,
@@ -124,7 +175,15 @@ namespace art {
 
     void MemMap::MadviseDontNeedAndZero()
     {
-        Genode::warning(__PRETTY_FUNCTION__, ": not implemented");
+        if (base_begin_ == nullptr && base_size_ == 0)
+        {
+            return;
+        }
+
+        if (!kMadviseZeroes)
+        {
+            Genode::memset(base_begin_, 0, base_size_);
+        }
     }
 
     void MemMap::AlignBy(size_t size)
