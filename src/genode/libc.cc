f1d1589d6b66da54500b7bcda7f3afedc01346ce
Alexander Senier <senier@componolit.com>
2019-09-20 16:30:12 +0200

    Raise exception on raise() call
    Ref. #64

--- src/genode/libc.cc
+++ src/genode/libc.cc
@@ -1,9 +1,13 @@
 #include <base/log.h>
+#include <base/exception.h>
+
 #include <signal.h>
 #include <errno.h>
 #include <sched.h>
 
 #define NOT_IMPLEMENTED Genode::warning(__func__, ": not implemented")
+
+class Raise : Genode::Exception { };
 
 extern "C" {
 
@@ -11,9 +15,16 @@ int sigwait(const sigset_t *, int *) { NOT_IMPLEMENTED; return EINVAL; }
 int sched_getscheduler(pid_t) { NOT_IMPLEMENTED; errno = EINVAL; return -1; }
 int sched_getparam(pid_t, struct sched_param *) { NOT_IMPLEMENTED; return -1; }
 
-void raise_SIGINT() {
-   NOT_IMPLEMENTED;
-   exit(1);
+int raise(int sig) {
+    switch (sig)
+    {
+        case SIGKILL:
+            Genode::error("Program killed by raise(SIGKILL)");
+            throw Raise();
+        default:
+            Genode::error("raise(", sig, ")");
+            return -1;
+    }
 }
 
 }
