a1c3d5034e910027ce373e710dd3f754ffe71c8a
Alexander Senier <senier@componolit.com>
2019-12-10 15:48:07 +0100

    Check for working tmpfile() in gtest_main

--- src/gtest/gtest_main.cc
+++ src/gtest/gtest_main.cc
@@ -1,7 +1,18 @@
+/* Genode includes */
+#include <base/log.h>
+
+/* libc include */
+#include <stdio.h>
+
 /* Googletest includes */
 #include <gtest/gtest.h>
 
 int main (int argc, const char **argv) {
-   testing::InitGoogleTest(&argc, (char **)argv);
+	FILE *f = tmpfile();
+	if (f == nullptr) {
+		Genode::error("Tmpfile not working (/tmp present and writeable?).");
+		exit(255);
+	}
+	testing::InitGoogleTest(&argc, (char **)argv);
 	return RUN_ALL_TESTS();
 }
