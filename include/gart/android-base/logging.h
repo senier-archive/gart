2c821577262b80a15aa1f6c0fc6c9c5ffa9c4c82
Alexander Senier <senier@componolit.com>
2019-12-30 00:43:00 +0100

    Execute conditions in mocked CHECK_OP
    ART seems to rely on the side effects of CHECK_* for initialization sometimes.

--- include/gart/android-base/logging.h
+++ include/gart/android-base/logging.h
@@ -7,7 +7,7 @@
 #ifdef ART_LOG_CONSTEXPR_HACK
 
 #undef CHECK_OP
-#define CHECK_OP(LHS, RHS, OP) Discard()
+#define CHECK_OP(LHS, RHS, OP) if (!((LHS) OP (RHS))) { std::cout << "Check failed at " << __FILE__ << " +" << __LINE__ << std::endl; abort(); }; Discard()
 
 #undef WOULD_LOG
 #define WOULD_LOG(severity) MUST_LOG_MESSAGE(severity)
