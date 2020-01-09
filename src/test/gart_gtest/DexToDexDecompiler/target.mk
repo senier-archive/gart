a7b00086e264d9f4010f0cdacb02006926dc705d
Alexander Senier <senier@componolit.com>
2020-01-09 10:52:41 +0100

    WIP: Support more libart-compiler tests

---
+++ src/test/gart_gtest/DexToDexDecompiler/target.mk
@@ -0,0 +1,3 @@
+GART_GTEST_NAME = DexToDexDecompiler
+SRC_JAVA = Main.java
+include $(call select_from_repositories,lib/mk/gart_gtest.inc)
---
+++ src/test/gart_gtest/StaticLeafMethods/target.mk
@@ -0,0 +1,2 @@
+GART_GTEST_NAME = StaticLeafMethods
+include $(call select_from_repositories,lib/mk/gart_gtest.inc)
