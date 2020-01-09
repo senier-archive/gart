1d6e14fa592610076bd59e968d2a751784922160
Alexander Senier <senier@componolit.com>
2020-01-09 16:04:18 +0100

    Fix libcrypto tests
    Closes #73, closes #72

--- lib/mk/gart_libcrypto.mk
+++ lib/mk/gart_libcrypto.mk
@@ -3,6 +3,10 @@ ANDROID_DIR			= boringssl
 ANDROID_SECTIONS	= /cc_library[@name=libcrypto] /cc_library[@name=libcrypto]/target/linux_$(call android_arch)
 ANDROID_BUILDFILES	= sources.bp Android.bp
 
+ANDROID_EXCLUDE_C += src/crypto/rand_extra/fuchsia.c
+ANDROID_EXCLUDE_C += src/crypto/rand_extra/windows.c
+ANDROID_EXCLUDE_C += src/crypto/rand_extra/deterministic.c
+
 # Some files cannot be built with clang and are treated specially.
 # No problem for us, as we use gcc.
 ANDROID_UNDEFINED = libcrypto_no_clang
