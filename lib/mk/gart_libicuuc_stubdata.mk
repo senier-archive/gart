03bd31b99febbe576c5d432833b169608d06dcf5
Alexander Senier <senier@componolit.com>
2020-01-10 12:36:59 +0100

    Generate tarball with ICU data

--- lib/mk/gart_libicuuc_stubdata.mk
+++ lib/mk/gart_libicuuc_stubdata.mk
@@ -5,3 +5,8 @@ ANDROID_SECTIONS	= /cc_library_static[@name=libicuuc_stubdata]
 include $(call select_from_repositories,lib/mk/android-lib.inc)
 
 SHARED_LIB =
+
+all: $(BUILD_BASE_DIR)/bin/icu.tar
+
+$(BUILD_BASE_DIR)/bin/icu.tar: $(ANDROID_BASEDIR)/stubdata/icudt*l.dat
+	$(VERBOSE)tar -cf $@ -C $(ANDROID_BASEDIR)/stubdata $(notdir $<)
