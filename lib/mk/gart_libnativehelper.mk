32b9948f45c4f8adbf0b7a50f1bf4f7faf7334e5
Alexander Senier <senier@componolit.com>
2020-01-10 12:37:50 +0100

    Add missing source files to libnativehelper

--- lib/mk/gart_libnativehelper.mk
+++ lib/mk/gart_libnativehelper.mk
@@ -1,8 +1,10 @@
 ANDROID_PORT		= libnativehelper
 ANDROID_DIR			= libnativehelper
-ANDROID_SECTIONS	= /cc_library[@name=libnativehelper]
+ANDROID_SECTIONS	= /cc_library[@name=libnativehelper] /cc_library[@name=libnativehelper]/target/linux
 ANDROID_INCLUDES	= include_jni platform_include header_only_include
 
 ANDROID_EXCLUDE_OPT += -fvisibility=protected
 
 include $(call select_from_repositories,lib/mk/android-lib.inc)
+
+CC_OPT += -D__SIGRTMIN=SIGRTMIN
