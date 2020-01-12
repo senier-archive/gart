ec5733d9ab4b37d590bc5fd3ac77bd19fdcedb85
Alexander Senier <senier@componolit.com>
2020-01-12 20:08:08 +0100

    Make abort() macro optional

--- include/gart/stdlib.h
+++ include/gart/stdlib.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#ifdef GART_ABORT_MACRO
 #include <stdio.h>
-
 #define abort gart_abort
-#include_next <stdlib.h>
-#undef abort
+#endif
 
+#include_next <stdlib.h>
+
+#ifdef GART_ABORT_MACRO
+#undef abort
 #include <gart_abort.h>
+#endif
