baba38493f69112950a4acf7db7743a2c0dee5e7
Alexander Senier <senier@componolit.com>
2020-01-12 11:00:48 +0100

    Port libart-simulator

---
+++ lib/mk/gart_libart-simulator.mk
@@ -0,0 +1,18 @@
+ANDROID_PORT       = art
+ANDROID_DIR        = art
+ANDROID_SOURCES    = simulator
+ANDROID_INCLUDES   = simulator simulator/include
+ANDROID_BUILDFILES = Android.bp test/Android.bp build/Android.bp simulator/Android.bp
+ANDROID_SECTIONS   = /art_cc_library[@name=libart-simulator]
+
+# jni.h required
+ANDROID_LIBS += libnativehelper
+
+# dlmalloc required
+ANDROID_LIBS += dlmalloc
+
+# libartbase required
+ANDROID_LIBS += libartbase
+
+include $(REP_DIR)/lib/mk/art_opts.inc
+include $(call select_from_repositories,lib/mk/android-lib.inc)
