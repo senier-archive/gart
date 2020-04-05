7095ef5d23feba9716d60bcd0e1388f0fd8b9343
Alexander Senier <senier@componolit.com>
2020-04-06 00:50:08 +0200

    Fix IPC client port

--- src/test/ipc_client/target.mk
+++ src/test/ipc_client/target.mk
@@ -2,9 +2,8 @@ TARGET = ipc_client
 
 ANDROID_PORT	 = binder_demo
 ANDROID_DIR		 = binder_demo
-ANDROID_SOURCES	 = libartbase
 ANDROID_SECTIONS = /cc_test[@name=ipc_client]
 
-LIBS += gtest gart_gtest_main
+LIBS += gtest gart_main gart_liblog
 
 include $(call select_from_repositories,lib/mk/android-prg.inc)
